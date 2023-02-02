c2d37b2c22e64fadc4e32671565aa2e85d09d288
Dima Ovharenko <dima-o@mail.ru>
2023-02-02 13:47:18 +0300

    add style

--- index.php
+++ index.php
@@ -5,28 +5,30 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
-	<title>Document</title>
+	<title>Export Data to Excel</title>
 </head>
 <body>
 	<div class="wrapper">
 		<h1 class="title">Export Data to Excel</h1>
-		<div class="export__box">
-			<form action="<?=$_SERVER['PHP_SELF'];?>" class="export__form" method="post">
-				<button type="submit" name="exportData" id="exportData" class="export__btn">Export Data to Excel</button>
-			</form>
+		<div class="export__content">
+			<div class="export__box">
+				<form action="<?=$_SERVER['PHP_SELF'];?>" class="export__form" method="post">
+					<button type="submit" name="exportData" id="exportData" class="export__btn">Export Data to Excel</button>
+				</form>
+			</div>
+			<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Age</th>	
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+				</tbody>
+			</table>
 		</div>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Age</th>	
-					<th>Email</th>
-				</tr>
-			</thead>
-			<tbody>
-
-			</tbody>
-		</table>
 	</div>
 </body>
 </html>
