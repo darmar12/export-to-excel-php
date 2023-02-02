52d52082569e131b75cb77f5fe5dfbf502784a21
Dima Ovharenko <dima-o@mail.ru>
2023-02-02 17:01:08 +0300

    add export data

--- export.php
+++ export.php
@@ -5,4 +5,21 @@ $table_tr = [];
 while( $row = mysqli_fetch_assoc($res) ) {
 	$table_tr[] = $row;
 }
+
+if(isset($_POST["exportData"])) {
+	$filename = date('Y-m-d') . ".xls";
+	$is_coloumn = false;
+	header("Content-Type: application/vnd.ms-excel");
+	header("Content-Disposition: attachment; filename=\"$filename\"");	
+	if(!empty($table_tr)) {
+		foreach($table_tr as $row) {
+			if(!$is_coloumn) {
+			  echo implode("\t", array_keys($row)) . "\n";
+			  $is_coloumn = true;
+			}
+			echo implode("\t", array_values($row)) . "\n";
+		}
+	}
+	exit;  
+}
 ?>
